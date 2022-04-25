a31d66de018be3500943f7b50b392c78e49cf79e
Chance Gardner <Chantz465@gmail.com>
2022-04-25 13:37:32 -0500

    first upload

---
+++ Operators1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Operators1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int numA = ;
+            //int numB = ;
+
+           
+
+            Console.WriteLine("Enter a number");
+            int numA = int.Parse(Console.ReadLine());
+            Console.WriteLine("You entered {0}", numA);
+
+
+            Console.WriteLine("Enter a number");
+            int numB = int.Parse(Console.ReadLine());
+            Console.WriteLine("You entered {0}", numB);
+
+
+            int add =  numA + numB;
+
+            Console.WriteLine("Add ", add);
+
+            int subtract = numA - numB;
+
+            Console.WriteLine("subtract ", subtract );
+
+            int divide = numA / numB;
+            int remainder = numA % numB;
+
+            Console.WriteLine("divide " + divide + " " + remainder);
+
+
+
+
+           
+
+
+        }
+    }
+}
---
+++ Operators2/Operators2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Operators2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("what is the radius of the circle");
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * Math.Pow(radius, 2);
+
+
+
+            Console.WriteLine("the area of a circle with a radius of " + radius + " is " + area);
+
+
+        }
+    }
+}
+
